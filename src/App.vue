f65ba8f0df46175acc24b34ad47abfa24ed2c5ea
Kosolapov Anatolii <trogwar4ik4@gmail.com>
2023-01-10 20:26:51 +0300

    Add saving to localStorage

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
 <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
+  <!-- <div class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center">
+    <svg class="animate-spin -ml-1 mr-3 h-12 w-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+    </svg>
+  </div> -->
   <div class="container">
     <div class="w-full my-4"></div>
     <section>
@@ -19,6 +25,21 @@
               placeholder="Например DOGE"
             />
           </div>
+          <div class="flex bg-white p-1 rounded-md shadow-md flex-wrap">
+            <span class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+              BTC
+            </span>
+            <span class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+              DOGE
+            </span>
+            <span class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+              BCH
+            </span>
+            <span class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+              CHD
+            </span>
+          </div>
+          <div class="text-sm text-red-600">Такой тикер уже добавлен</div>
         </div>
       </div>
       <button
@@ -136,29 +157,46 @@ export default {
     return{
       ticker: "",
       tickers: [],
-        sel: null,
-        graph: []
+      sel: null,
+      graph: [],
     };
+  },
+
+  created(){
+    const tickersData = localStorage.getItem("cryptonomicon-list");
+    if(tickersData){
+      this.tickers = JSON.parse(tickersData);
+      this.tickers.forEach(ticker => {
+        this.subscribeToUpdates(ticker.name)
+      });
+    }
   },
 
 
   methods: {
+    subscribeToUpdates(tickerName){
+      setInterval(async()=>{
+        const f = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=2d0ec3d247245897757f871161e78b96c07105d990c860fb649f767b3bba8f66`);
+        const data = await f.json();
+        this.tickers.find(t=> t.name === tickerName).price = data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+        if (this.sel?.name === tickerName){
+          this.graph.push(data.USD)
+        }
+      },8000);
+      this.ticker = "";
+    },
+
+
     add(){
       const currentTicker = {
         name: this.ticker,
         price: "-"
       };
       this.tickers.push(currentTicker);
-      setInterval(async()=>{
-        const f = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_key=2d0ec3d247245897757f871161e78b96c07105d990c860fb649f767b3bba8f66`);
-        const data = await f.json();
-        this.tickers.find(t=> t.name === currentTicker.name).price = data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
-        if (this.sel?.name === currentTicker.name){
-          this.graph.push(data.USD)
-        }
-      },8000);
-      this.ticker = "";
+      localStorage.setItem('cryptonomicon-list', JSON.stringify(this.tickers));
+      this.subscribeToUpdates(currentTicker.name);
     },
+
     select(ticker){
       this.sel = ticker;
       this.graph = [];
