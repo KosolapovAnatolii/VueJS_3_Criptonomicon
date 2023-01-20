9b2be539c550b28072e78eb6fb9ee9512d545c2c
Kosolapov Anatolii <trogwar4ik4@gmail.com>
2023-01-20 16:11:22 +0300

    return 4 repeat stading

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <template>
   <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
-    <!-- <div class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center">
-    <svg class="animate-spin -ml-1 mr-3 h-12 w-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-    </svg>
-  </div> -->
     <div class="container">
       <div class="w-full my-4"></div>
       <add-ticker @add-ticker="add" :disabled="tooManyTickersAdded" />
@@ -20,23 +14,22 @@
             Назад
           </button>
           <button
-            class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
             @click="page = page + 1"
-            v-if="nextPage"
+            v-if="hasNextPage"
           >
-            Вперёд
+            Вперед
           </button>
           <div>Фильтр: <input v-model="filter" /></div>
         </div>
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            v-for="t of paginatedTickers"
+            v-for="t in paginatedTickers"
             :key="t.name"
             @click="select(t)"
             :class="{
-              'border-4': selectedTicker === t,
-              'bg-red-200': t.price === '-',
+              'border-4': selectedTicker === t
             }"
             class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
           >
@@ -71,7 +64,7 @@
         </dl>
         <hr class="w-full border-t border-gray-600 my-4" />
       </template>
-      <section class="relative" v-if="selectedTicker">
+      <section v-if="selectedTicker" class="relative">
         <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
           {{ selectedTicker.name }} - USD
         </h3>
@@ -81,7 +74,7 @@
         >
           <div
             v-for="(bar, idx) in normalizedGraph"
-            ref="graphElement"
+            ref="graphWidth"
             :key="idx"
             :style="{ height: `${bar}%` }"
             class="bg-purple-800 border w-10"
@@ -102,7 +95,7 @@
             x="0"
             y="0"
             viewBox="0 0 511.76 511.76"
-            style="enable-background: new 0 0 512 512"
+            style="enable-background:new 0 0 512 512"
             xml:space="preserve"
           >
             <g>
@@ -120,7 +113,8 @@
 </template>
 
 <script>
-// import { response } from 'express';
+
+
 import { subscribeToTicker, unsubscribeFromTicker } from "./api";
 import AddTicker from "./components/AddTicker.vue";
 
@@ -128,18 +122,19 @@ export default {
   name: "App",
 
   components: {
-    AddTicker,
+    AddTicker
   },
 
   data() {
     return {
+      filter: "",
+
       tickers: [],
       selectedTicker: null,
+
       graph: [],
       page: 1,
-      filter: "",
-      maxGraphElements: 1,
-      graphElementWidth: 38,
+      maxGraphElements: 1,  
     };
   },
 
@@ -150,26 +145,18 @@ export default {
 
     const VALID_KEYS = ["filter", "page"];
 
-    VALID_KEYS.forEach((key) => {
+    VALID_KEYS.forEach(key => {
       if (windowData[key]) {
         this[key] = windowData[key];
       }
     });
 
-    // if(windowData.filter) {
-    //   this.filter = windowData.filter;
-    // }
-
-    // if(windowData.page) {
-    //   this.page = windowData.page;
-    // }
-
     const tickersData = localStorage.getItem("cryptonomicon-list");
 
     if (tickersData) {
       this.tickers = JSON.parse(tickersData);
-      this.tickers.forEach((ticker) => {
-        subscribeToTicker(ticker.name, (newPrice) =>
+      this.tickers.forEach(ticker => {
+        subscribeToTicker(ticker.name, newPrice =>
           this.updateTicker(ticker.name, newPrice)
         );
       });
@@ -179,15 +166,15 @@ export default {
   },
 
   mounted() {
-    window.addEventListener("resize", this.calculateMaxGraphEements);
+    window.addEventListener("resize", this.calculateMaxGraphElements);
   },
 
   beforeUnmount() {
-    window.removeEventListener("resize", this.calculateMaxGraphEements);
+    window.removeEventListener("resize", this.calculateMaxGraphElements);
   },
 
   computed: {
-    tooManyTickersAdded(){
+    tooManyTickersAdded() {
       return this.tickers.length > 4;
     },
 
@@ -200,14 +187,14 @@ export default {
     },
 
     filteredTickers() {
-      return this.tickers.filter((ticker) => ticker.name.includes(this.filter));
+      return this.tickers.filter(ticker => ticker.name.includes(this.filter));
     },
 
     paginatedTickers() {
       return this.filteredTickers.slice(this.startIndex, this.endIndex);
     },
 
-    nextPage() {
+    hasNextPage() {
       return this.filteredTickers.length > this.endIndex;
     },
 
@@ -220,31 +207,31 @@ export default {
       }
 
       return this.graph.map(
-        (price) => 5 + ((price - minValue) * 95) / (maxValue - minValue)
+        price => 5 + ((price - minValue) * 95) / (maxValue - minValue)
       );
     },
 
     pageStateOptions() {
       return {
         filter: this.filter,
-        page: this.page,
+        page: this.page
       };
-    },
+    }
   },
 
   methods: {
-    calculateMaxGraphEements() {
+    calculateMaxGraphElements() {
       if (!this.$refs.graph) {
         return;
       }
       this.maxGraphElements =
-        this.$refs.graph.clientWidth / this.graphElementWidth;
+        this.$refs.graph.clientWidth / this.$refs.graphWidth[0].clientWidth;
     },
 
     updateTicker(tickerName, price) {
       this.tickers
-        .filter((t) => t.name === tickerName)
-        .forEach((t) => {
+        .filter(t => t.name === tickerName)
+        .forEach(t => {
           if (t === this.selectedTicker) {
             this.graph.push(price);
             while (this.graph.length > this.maxGraphElements) {
@@ -262,25 +249,15 @@ export default {
       return price > 1 ? price.toFixed(2) : price.toPrecision(2);
     },
 
-    async updateTickers() {
-      // if(!this.tickers.length){
-      //   return;
-      // }
-      // this.tickers.forEach(ticker => {
-      //   const price = exchangeData[ticker.name.toUpperCase()];
-      //   ticker.price = price ?? "-";
-      // });
-    },
-
     add(ticker) {
       const currentTicker = {
         name: ticker,
-        price: "-",
+        price: "-"
       };
 
       this.tickers = [...this.tickers, currentTicker];
       this.filter = "";
-      subscribeToTicker(currentTicker.name, (newPrice) =>
+      subscribeToTicker(currentTicker.name, newPrice =>
         this.updateTicker(currentTicker.name, newPrice)
       );
     },
@@ -290,19 +267,19 @@ export default {
     },
 
     handleDelete(tickerToRemove) {
-      this.tickers = this.tickers.filter((t) => t !== tickerToRemove);
+      this.tickers = this.tickers.filter(t => t !== tickerToRemove);
       if (this.selectedTicker === tickerToRemove) {
         this.selectedTicker = null;
       }
       unsubscribeFromTicker(tickerToRemove.name);
-    },
+    }
   },
 
   watch: {
     selectedTicker() {
       this.graph = [];
-      this.$nextTick().then(this.calculateMaxGraphEements);
-      // this.$nextTick().then(this.graphElementWidth = this.$refs.graphElement[0].clientWidth);
+
+      this.$nextTick().then(this.calculateMaxGraphElements);
     },
 
     tickers() {
@@ -319,14 +296,13 @@ export default {
       this.page = 1;
     },
 
-    pageStateOptions(v) {
+    pageStateOptions(value) {
       window.history.pushState(
         null,
         document.title,
-        `${window.location.pathname}?filter=${v.filter}&page=${v.page}`
+        `${window.location.pathname}?filter=${value.filter}&page=${value.page}`
       );
-    },
-  },
+    }
+  }
 };
-// №21 Криптономикон 00:00
 </script>
